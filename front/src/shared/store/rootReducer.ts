9408abfcec4e9bf7e6adf48400f1fd0a6ab87b57
kiberchainik <ya.sacher@gmail.com>
2024-11-03 19:03:57 +0100

    fix store

--- front/src/shared/store/rootReducer.ts
+++ front/src/shared/store/rootReducer.ts
@@ -1,10 +1,8 @@
 import userReducer from "@/features/userHeaderBtn/slice/userSlice";
 import { combineReducers } from "@reduxjs/toolkit"
 
-const rootReducer = combineReducers({
+export const rootReducer = combineReducers({
   user: userReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
-
-export default rootReducer
