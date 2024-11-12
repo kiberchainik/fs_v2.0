4b2af0e035a9c041cf34a127f8a8cd464e017855
kiberchainik <ya.sacher@gmail.com>
2024-11-12 01:34:37 +0100

    fix client side error

--- front/src/app/(root)/offerta/[jobSlug]/JobFullPage.tsx
+++ front/src/app/(root)/offerta/[jobSlug]/JobFullPage.tsx
@@ -1,15 +1,18 @@
 'use client'
 
 import { IVacanciaesFullDate } from "@/features/agency/vacancy/types";
+import CarouselJobs from "@/features/carousel/components/Carousel";
 import VacancyPage from "@/features/vacancy/components/fullVacancy/VacancyPage";
 import { Card } from "@/shared/components";
 
 export default function JobFullPage(jobData: IVacanciaesFullDate) {
   return (
-    <Card className='flex flex-col gap-y-3 p-5'>
-      <VacancyPage {...jobData} />
-      {/** Contacnt form */}
-      {/** Simular vacancies */}
-    </Card>
+    <>
+      <Card className='flex flex-col gap-y-3 p-5'>
+        <VacancyPage {...jobData} />
+        {/** Contacnt form */}
+      </Card>
+      {/* <CarouselJobs /> */}
+    </>
   );
 }
--- front/src/features/vacancy/components/fullVacancy/VacancyPage.tsx
+++ front/src/features/vacancy/components/fullVacancy/VacancyPage.tsx
@@ -2,7 +2,7 @@ import { IVacanciaesFullDate } from "@/features/agency/vacancy/types"
 import { Button, Heading } from "@/shared/components"
 
 import styles from './vacancyPage.module.scss'
-import { CiCalendarDate, CiHeart, CiRead } from "react-icons/ci";
+import { CiCalendarDate, CiCirclePlus, CiHeart, CiRead } from "react-icons/ci";
 import { formatDate } from "@/shared/utils";
 import { useAppSelector } from "@/shared/hooks";
 import Link from "next/link";
@@ -26,6 +26,10 @@ export default function VacancyPage({ id, title, description, views, createdAt, 
     console.log('authUser email = ' + authUser);
   }
 
+  const sendCandudature = () => {
+    console.log('job id = ' + id)
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.wrapperVacancy}>
@@ -47,9 +51,12 @@ export default function VacancyPage({ id, title, description, views, createdAt, 
             <div className={styles.createdAt}><span><CiCalendarDate /></span> {formatDate(createdAt)}</div>
             {reallyUpTo && <div className={styles.reallyUpTo}>Attuale fino {formatDate(reallyUpTo)}</div>}
           </div>
-          <div className={styles.saveFavorite}>
+          <div className={styles.favoriteCandidat}>
             <Button onClick={() => saveInFavorite()}>
               <CiHeart />
+            </Button>
+            <Button onClick={() => sendCandudature()}>
+              <CiCirclePlus /> Candidati
             </Button>
           </div>
         </div>
@@ -59,6 +66,11 @@ export default function VacancyPage({ id, title, description, views, createdAt, 
       </div>
       <div className={styles.authorSector}>
         <VacancyCardAuthorInfo {...author} />
+        <div className={styles.authorSector_btn}>
+          <Button variant='default'>
+            <Link href={'#'}>Altri offerte</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
