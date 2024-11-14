526f5ece57d54c326426b42f56f4c80e9a305555
kiberchainik <ya.sacher@gmail.com>
2024-11-15 00:09:56 +0100

    upd main (server)

--- server/src/auth/strategies/google.strategy.ts
+++ server/src/auth/strategies/google.strategy.ts
@@ -6,7 +6,9 @@ import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20'
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 	constructor(private configService: ConfigService) {
+		console.log(configService.get('APP_URL') + '/api/auth/google/callback');
 		super({
+
 			clientID: configService.get('GOOGLE_CLIENT_ID'),
 			clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
 			callbackURL: configService.get('APP_URL') + '/api/auth/google/callback',
@@ -21,7 +23,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 		done: VerifyCallback //при успешном запросе
 	) {
 		const { displayName, emails, photos } = profile
-
+		console.log(this.configService.get('APP_URL') + '/api/auth/google/callback');
 		const user = {
 			email: emails[0].value,
 			name: displayName,
