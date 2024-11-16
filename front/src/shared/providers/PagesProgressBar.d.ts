c4ae0f27758db51954eb06c5cce89b608b2e7303
Alexandr <ya.sacher@gmail.com>
2024-11-16 09:13:35 +0100

    Update PagesProgressBar.d.ts

--- front/src/shared/providers/PagesProgressBar.d.ts
+++ front/src/shared/providers/PagesProgressBar.d.ts
@@ -1,3 +1,3 @@
 import React from 'react';
-import { ProgressBarProps } from '.';
+import { ProgressBarProps } from './index.d.ts';
 export declare const PagesProgressBar: React.MemoExoticComponent<({ color, height, options, shallowRouting, disableSameURL, startPosition, delay, stopDelay, style, nonce, }: Omit<ProgressBarProps, 'targetPreprocessor' | 'disableAnchorClick'>) => React.JSX.Element>;
