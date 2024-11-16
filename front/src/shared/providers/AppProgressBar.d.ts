8d7f92f047b726a795386ab7c5960e2cff5c1f1c
Alexandr <ya.sacher@gmail.com>
2024-11-16 09:13:10 +0100

    Update AppProgressBar.d.ts

--- front/src/shared/providers/AppProgressBar.d.ts
+++ front/src/shared/providers/AppProgressBar.d.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ProgressBarProps, RouterNProgressOptions } from '.';
+import { ProgressBarProps, RouterNProgressOptions } from './index.d.ts';
 import { type AppRouterInstance, NavigateOptions } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 export declare const AppProgressBar: React.MemoExoticComponent<({ color, height, options, shallowRouting, disableSameURL, startPosition, delay, stopDelay, style, nonce, targetPreprocessor, disableAnchorClick, }: ProgressBarProps) => React.JSX.Element>;
 export declare function useRouter(customRouter?: () => AppRouterInstance): {
