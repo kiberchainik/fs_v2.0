4f2d04a62dc9eaa85ce4557f0a5582401a4fcfc9
kiberchainik <ya.sacher@gmail.com>
2024-11-17 04:54:17 +0100

    upd search header

--- front/src/features/headerSearch/components/Search.tsx
+++ front/src/features/headerSearch/components/Search.tsx
@@ -4,16 +4,22 @@ import { Button, Input } from '@/shared/components'
 import styles from './search.module.scss'
 import { CiSearch } from 'react-icons/ci'
 import { useRef, useState } from 'react'
-import { useClickAway } from 'react-use'
+import { useClickAway, useDebounce } from 'react-use'
 import { cn } from '@/shared/utils'
+import Link from 'next/link'
 
 export function HeaderSearch() {
     const [focused, setFocused] = useState<boolean>(false)
+    const [searchQuery, setSearchQuery] = useState<string>('')
     const ref = useRef(null)
 
     useClickAway(ref, () => {
         setFocused(false)
     })
+
+    useDebounce(() => {
+
+    }, 1000, [searchQuery])
 
     return (
         <>
@@ -22,10 +28,12 @@ export function HeaderSearch() {
                 <Button variant='ghost' className={styles.searchIcon} onClick={() => console.log('searching ...')}>
                     <CiSearch />
                 </Button>
-                <Input type='text' placeholder='Search ...' onFocus={() => setFocused(true)} />
+                <Input type='text' placeholder='Search ...' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onFocus={() => setFocused(true)} />
             </div>
             <div className={cn(styles.searchResult, focused && 'visible opacity-100 top-12')}>
-
+                <Link href='' className={styles.elementResult}>
+                    <span>Element 1</span>
+                </Link>
             </div>
         </>
     )
