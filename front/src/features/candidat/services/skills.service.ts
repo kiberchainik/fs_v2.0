97e5ea5420a3f316beb13038f3ffb50ae356611c
Alexandr <ya.sacher@gmail.com>
2024-11-28 11:51:27 +0100

    Update services

---
+++ front/src/features/candidat/services/skills.service.ts
@@ -0,0 +1,18 @@
+import { ISkills } from "@/features/candidat/types";
+import { axiosPrivate } from "@/shared/api";
+import { TypeSkillsSchema } from "../schemes";
+import { API_URL } from "@/shared/config";
+
+class UserService {
+    public async getSkills() {
+        const { data } = await axiosPrivate.get<IPrivacy>(API_URL.candidatSkills())
+
+        return data
+    }
+
+    public async updateProfile(data: TypePrivacySchema) {
+        return await axiosPrivate.patch<IUser>(API_URL.candidatProfile(), data)
+    }
+}
+
+export const userService = new UserService()
