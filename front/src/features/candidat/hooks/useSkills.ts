6bb4bb881a24f27c01a6f7b00cadeda0971128e7
Alexandr <ya.sacher@gmail.com>
2024-11-28 11:35:00 +0100

    Update hooks

---
+++ front/src/features/candidat/hooks/useSkills.ts
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { skillsService } from "../services";
+import { useMemo } from "react";
+
+export function useSkills() {
+    const { data: skills, isFetching } = useQuery({
+        queryKey: ['get candidat skills'],
+        queryFn: () => skillsService.getSkills()
+    })
+
+    return useMemo(() => ({
+        skills,
+        isFetching
+    }), [privacy, isFetching])
+}
