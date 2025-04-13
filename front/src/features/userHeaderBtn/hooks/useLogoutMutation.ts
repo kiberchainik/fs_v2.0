f4f19d29ad50fbf289db44146de90359cbcbb30a
kiberchainik <ya.sacher@gmail.com>
2025-04-13 21:41:07 +0200

    upd front

--- front/src/features/userHeaderBtn/hooks/useLogoutMutation.ts
+++ front/src/features/userHeaderBtn/hooks/useLogoutMutation.ts
@@ -6,6 +6,7 @@ import { toastMessageHandler } from "@/shared/utils";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation"
 import { setLoading, setUser } from "../slice/userSlice";
+import { MAIN_URL } from "@/shared/config";
 
 export function useLogoutMutation() {
     const router = useRouter()
@@ -17,7 +18,8 @@ export function useLogoutMutation() {
         onSuccess() {
             dispatch(setLoading(true))
             dispatch(setUser(null))
-            router.push('/')
+            router.refresh()
+            router.push(MAIN_URL.home())
         },
         onError(error) {
             toastMessageHandler(error)
